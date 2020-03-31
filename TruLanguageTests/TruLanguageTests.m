f87de302e40f4fbb752d9620833c5079b4845c2b
Huy Vo <voxqhuy@gmail.com>
2020-03-31 12:31:04 -0400

    finish testing truSubstitute

--- TruLanguageTests/TruLanguageTests.m
+++ TruLanguageTests/TruLanguageTests.m
@@ -85,62 +85,132 @@
     XCTAssertEqual([_sut truLookup:@"my-xor" with:group3], xorry);
     XCTAssertEqual([_sut truLookup:@"my-majority" with:@[majy]], majy);
     XCTAssertEqual([_sut truLookup:@"my-majority" with:group1], majy);
-    XCTAssertNil([_sut truLookup:@"my-not" with:nil]);
+    XCTAssertNil  ([_sut truLookup:@"my-not" with:nil]);
 }
 
 - (void)testTruSubstitute {
     // GIVEN
     TruValue *falseVal = [[TruValue alloc] init:false];
-    TruValue *trueVal = [[TruValue alloc] init:true];
+    TruValue *trueVal =  [[TruValue alloc] init:true];
     TruId *x = [[TruId alloc] init:@"x"];
     TruId *y = [[TruId alloc] init:@"y"];
-    TruNot *notX = [[TruNot alloc] init:[[TruId alloc] init:@"x"]];
-    TruAnd *andXY = [[TruAnd alloc] initWithLhs:[[TruId alloc] init:@"x"] andRhs:[[TruId alloc] init:@"y"]];
-    TruOr *orXY = [[TruOr alloc] initWithLhs:[[TruId alloc] init:@"x"] andRhs:[[TruId alloc] init:@"y"]];
-    TruNand *nandXY = [[TruNand alloc] initWithLhs:[[TruId alloc] init:@"x"] andRhs:[[TruId alloc] init:@"y"]];
-    TruNor *norXY = [[TruNor alloc] initWithLhs:[[TruId alloc] init:@"x"] andRhs:[[TruId alloc] init:@"y"]];
-    TruXor *xorXY = [[TruXor alloc] initWithLhs:[[TruId alloc] init:@"x"] andRhs:[[TruId alloc] init:@"y"]];
-    TruXnor *xnorXY = [[TruXnor alloc] initWithLhs:[[TruId alloc] init:@"x"] andRhs:[[TruId alloc] init:@"y"]];
-    TruImply *implyXY = [[TruImply alloc] initWithLhs:[[TruId alloc] init:@"x"] andRhs:[[TruId alloc] init:@"y"]];
-    TruEqual *equalXY = [[TruEqual alloc] initWithLhs:[[TruId alloc] init:@"x"] andRhs:[[TruId alloc] init:@"y"]];
-    TruMaj *majXYZ = [[TruMaj alloc] initWithFirst:[[TruId alloc] init:@"x"] second:[[TruId alloc] init:@"y"] andThird:[[TruId alloc] init:@"z"]];
+    TruId *z = [[TruId alloc] init:@"z"];
+    TruNot *notX =      [[TruNot   alloc] init:x];
+    TruAnd *andXY =     [[TruAnd   alloc] initWithLhs:x andRhs:y];
+    TruOr *orXY =       [[TruOr    alloc] initWithLhs:x andRhs:y];
+    TruNand *nandXY =   [[TruNand  alloc] initWithLhs:x andRhs:y];
+    TruNor *norXY =     [[TruNor   alloc] initWithLhs:x andRhs:y];
+    TruXor *xorXY =     [[TruXor   alloc] initWithLhs:x andRhs:y];
+    TruXnor *xnorXY =   [[TruXnor  alloc] initWithLhs:x andRhs:y];
+    TruImply *implyXY = [[TruImply alloc] initWithLhs:x andRhs:y];
+    TruEqual *equalXY = [[TruEqual alloc] initWithLhs:x andRhs:y];
+    TruMaj *majXYZ =    [[TruMaj   alloc] initWithFirst:x second:y andThird:z];
+    TruCall *myFalse =  [[TruCall  alloc] initWithFunction:@"my-false" andExpressions:nil];
+    TruCall *myNot =    [[TruCall  alloc] initWithFunction:@"my-not" andExpressions:@[x]];
+    TruCall *myXor =    [[TruCall  alloc] initWithFunction:@"my-xor" andExpressions:@[x, y]];
+    TruCall *myMaj =    [[TruCall  alloc] initWithFunction:@"my-majority" andExpressions:@[x, y, z]];
     
     // WHEN
-    TruExpr *notFalse = [_sut truSubstitute:falseVal in:@"x" for:notX];
-    TruExpr *andTrueY = [_sut truSubstitute:trueVal in:@"x" for:andXY];
-    TruExpr *orXFalse = [_sut truSubstitute:falseVal in:@"y" for:orXY];
-    TruExpr *nandTrueY = [_sut truSubstitute:trueVal in:@"x" for:nandXY];
-    TruExpr *norXTrue = [_sut truSubstitute:trueVal in:@"y" for:norXY];
-    TruExpr *xorFalseY = [_sut truSubstitute:falseVal in:@"x" for:xorXY];
-    TruExpr *xnorXFalse = [_sut truSubstitute:falseVal in:@"y" for:xnorXY];
+    TruExpr *notFalse =    [_sut truSubstitute:falseVal in:@"x" for:notX];
+    TruExpr *andTrueY =    [_sut truSubstitute:trueVal  in:@"x" for:andXY];
+    TruExpr *orXFalse =    [_sut truSubstitute:falseVal in:@"y" for:orXY];
+    TruExpr *nandTrueY =   [_sut truSubstitute:trueVal  in:@"x" for:nandXY];
+    TruExpr *norXTrue =    [_sut truSubstitute:trueVal  in:@"y" for:norXY];
+    TruExpr *xorFalseY =   [_sut truSubstitute:falseVal in:@"x" for:xorXY];
+    TruExpr *xnorXFalse =  [_sut truSubstitute:falseVal in:@"y" for:xnorXY];
+    TruExpr *implyFalseY = [_sut truSubstitute:falseVal in:@"x" for:implyXY];
+    TruExpr *equalXTrue =  [_sut truSubstitute:trueVal  in:@"y" for:equalXY];
+    TruExpr *majXYFalse =  [_sut truSubstitute:falseVal in:@"z" for:majXYZ];
+    TruExpr *trueMyFalse = [_sut truSubstitute:trueVal in:@"x" for:myFalse];
+    TruExpr *falseXMyNot = [_sut truSubstitute:falseVal in:@"x" for:myNot];
+    TruExpr *trueYMyXor =  [_sut truSubstitute:trueVal in:@"y" for:myXor];
+    TruExpr *falseZMyMaj = [_sut truSubstitute:falseVal in:@"z" for:myMaj];
     
     // THEN
     XCTAssertEqual([_sut truSubstitute:trueVal in:@"x" for:falseVal], falseVal);
     XCTAssertEqual([_sut truSubstitute:trueVal in:@"x" for:x], trueVal);
     XCTAssertEqual([_sut truSubstitute:trueVal in:@"x" for:y], y);
     XCTAssertEqual([(TruValue*) [(TruNot*) notFalse expression] value], false);
-    XCTAssertEqual([(TruId*) [(TruAnd*) andTrueY rhs] name], @"y");
-    XCTAssertEqual([(TruId*) [(TruOr*) orXFalse lhs] name], @"x");
-    XCTAssertEqual([(TruId*) [(TruNand*) nandTrueY rhs] name], @"y");
-    XCTAssertEqual([(TruId*) [(TruNor*) norXTrue lhs] name], @"x");
-    XCTAssertEqual([(TruId*) [(TruXor*) xorFalseY rhs] name], @"y");
-    XCTAssertEqual([(TruId*) [(TruXnor*) xnorXFalse lhs] name], @"x");
-    
-    //  (test (tru-substitute (tru-parse-expression `false) 'x (tru-parse-expression `{implies x y}))
-    //        (tru-parse-expression `{implies false y}))
-    //  (test (tru-substitute (tru-parse-expression `true)  'y (tru-parse-expression `{equals x y}))
-    //        (tru-eq (tru-id 'x) (tru-value #t)))
-    //  (test (tru-substitute (tru-parse-expression `false) 'z (tru-parse-expression `{majority x y z}))
-    //        (tru-parse-expression `{majority x y false}))
+    XCTAssertEqual([(TruId*)    [(TruAnd*) andTrueY rhs] name], @"y");
+    XCTAssertEqual([(TruId*)    [(TruOr*) orXFalse lhs] name], @"x");
+    XCTAssertEqual([(TruId*)    [(TruNand*) nandTrueY rhs] name], @"y");
+    XCTAssertEqual([(TruId*)    [(TruNor*) norXTrue lhs] name], @"x");
+    XCTAssertEqual([(TruId*)    [(TruXor*) xorFalseY rhs] name], @"y");
+    XCTAssertEqual([(TruId*)    [(TruXnor*) xnorXFalse lhs] name], @"x");
+    XCTAssertEqual([(TruId*)    [(TruImply*) implyFalseY rhs] name], @"y");
+    XCTAssertEqual([(TruId*)    [(TruXnor*) xnorXFalse lhs] name], @"x");
+    XCTAssertEqual([(TruId*)    [(TruEqual*) equalXTrue lhs] name], @"x");
+    XCTAssertEqual([(TruId*)    [(TruMaj*) majXYFalse first] name], @"x");
+    XCTAssertEqual([(TruId*)    [(TruMaj*) majXYFalse second] name], @"y");
+    XCTAssertEqual([(TruCall*)  trueMyFalse function], @"my-false");
+    XCTAssertEqual([(TruCall*)  falseXMyNot function], @"my-not");
+    XCTAssertEqual([(TruValue*) [(TruCall*) falseXMyNot arguments][0] value], false);
+    XCTAssertEqual([(TruCall*)  trueYMyXor function], @"my-xor");
+    XCTAssertEqual([(TruId*)    [(TruCall*) trueYMyXor arguments][0] name], @"x");
+    XCTAssertEqual([(TruCall*)  falseZMyMaj function], @"my-majority");
+    XCTAssertEqual([(TruId*)    [(TruCall*) falseZMyMaj arguments][0] name], @"x");
+    XCTAssertEqual([(TruId*)    [(TruCall*) falseZMyMaj arguments][1] name], @"y");
+    XCTAssertNil([_sut truSubstitute:trueVal in:@"x" for:nil]);
+}
 
-    //  (test (tru-substitute (tru-parse-expression `true)  'x (tru-parse-expression `{my-false}))
-    //        (tru-parse-expression `{my-false}))
-    //  (test (tru-substitute (tru-parse-expression `false) 'x (tru-parse-expression `{my-not x}))
-    //        (tru-call1 'my-not (tru-value #f)))
-    //  (test (tru-substitute (tru-parse-expression `true)  'y (tru-parse-expression `{my-xor x y}))
-    //        (tru-parse-expression `{my-xor x true}))
-    //  (test (tru-substitute (tru-parse-expression `false) 'z (tru-parse-expression `{my-majority x y z}))
-    //        (tru-call3 'my-majority (tru-id 'x) (tru-id 'y) (tru-value #f)))
+- (void)testTruInterpret
+{
+    // GIVEN
+    TruValue *falseVal = [[TruValue alloc] init:false];
+    TruValue *trueVal =  [[TruValue alloc] init:true];
+    TruId    *x =        [[TruId    alloc] init:@"x"];
+    TruNot   *notTrue =  [[TruNot   alloc] init:trueVal];
+    TruNot   *notFalse = [[TruNot   alloc] init:falseVal];
+    TruAnd   *andFF =    [[TruAnd   alloc] initWithLhs:falseVal andRhs:falseVal];
+    TruAnd   *andTF =    [[TruAnd   alloc] initWithLhs:trueVal andRhs:falseVal];
+    TruAnd   *andTT =    [[TruAnd   alloc] initWithLhs:trueVal andRhs:trueVal];
+    TruAnd   *andAnd =   [[TruAnd   alloc] initWithLhs:andTF andRhs:trueVal];
+    TruOr    *orFF =     [[TruOr    alloc] initWithLhs:falseVal andRhs:falseVal];
+    TruOr    *orTF =     [[TruOr    alloc] initWithLhs:trueVal andRhs:falseVal];
+    TruOr    *orTT =     [[TruOr    alloc] initWithLhs:trueVal andRhs:trueVal];
+    TruOr    *orOr =     [[TruOr    alloc] initWithLhs:orTF andRhs:falseVal];
+    TruNand  *nandFF =   [[TruNand  alloc] initWithLhs:falseVal andRhs:falseVal];
+    TruNand  *nandTF =   [[TruNand  alloc] initWithLhs:trueVal andRhs:falseVal];
+    TruNand  *nandTT =   [[TruNand  alloc] initWithLhs:trueVal andRhs:trueVal];
+    TruNand  *nandNand = [[TruNand  alloc] initWithLhs:nandTF andRhs:trueVal];
+    TruNor   *norFF =    [[TruNor   alloc] initWithLhs:falseVal andRhs:falseVal];
+    TruNor   *norTF =    [[TruNor   alloc] initWithLhs:trueVal andRhs:falseVal];
+    TruNor   *norTT =    [[TruNor   alloc] initWithLhs:trueVal andRhs:trueVal];
+    TruNor   *norNor =   [[TruNor   alloc] initWithLhs:norTF andRhs:trueVal];
+    TruXor   *xorFF =    [[TruXor   alloc] initWithLhs:falseVal andRhs:falseVal];
+    TruXor   *xorTF =    [[TruXor   alloc] initWithLhs:trueVal andRhs:falseVal];
+    TruXor   *xorTT =    [[TruXor   alloc] initWithLhs:trueVal andRhs:trueVal];
+    TruXor   *xorXOr =   [[TruXor   alloc] initWithLhs:xorTF andRhs:trueVal];
+    TruXnor  *xnorFF =   [[TruXnor  alloc] initWithLhs:falseVal andRhs:falseVal];
+    TruXnor  *xnorTF =   [[TruXnor  alloc] initWithLhs:trueVal andRhs:falseVal];
+    TruXnor  *xnorTT =   [[TruXnor  alloc] initWithLhs:trueVal andRhs:trueVal];
+    TruXnor  *xnorXnor = [[TruXnor  alloc] initWithLhs:xnorTF andRhs:trueVal];
+    TruImply *implyFF =  [[TruImply alloc] initWithLhs:falseVal andRhs:falseVal];
+    TruImply *implyTF =  [[TruImply alloc] initWithLhs:trueVal andRhs:falseVal];
+    TruImply *implyTT =  [[TruImply alloc] initWithLhs:trueVal andRhs:trueVal];
+    TruImply *impImply = [[TruImply alloc] initWithLhs:implyTF andRhs:trueVal];
+    TruEqual *eqFF =     [[TruEqual alloc] initWithLhs:falseVal andRhs:falseVal];
+    TruEqual *eqTF =     [[TruEqual alloc] initWithLhs:trueVal andRhs:falseVal];
+    TruEqual *eqTT =     [[TruEqual alloc] initWithLhs:trueVal andRhs:trueVal];
+    TruEqual *eqEq =     [[TruEqual alloc] initWithLhs:eqTF andRhs:trueVal];
+    TruMaj *majFFF =     [[TruMaj   alloc] initWithFirst:falseVal second:falseVal andThird:falseVal];
+    TruMaj *majTTT =     [[TruMaj   alloc] initWithFirst:trueVal second:trueVal andThird:trueVal];
+    TruMaj *majFFT =     [[TruMaj   alloc] initWithFirst:falseVal second:falseVal andThird:trueVal];
+    TruMaj *majMaj =     [[TruMaj   alloc] initWithFirst:majFFT second:trueVal andThird:falseVal];
+    TruCall *myFalse =   [[TruCall  alloc] initWithFunction:@"my-false" andExpressions:nil];
+    TruCall *myNot =     [[TruCall  alloc] initWithFunction:@"my-not" andExpressions:@[falseVal]];
+    TruCall *myXor =     [[TruCall  alloc] initWithFunction:@"my-xor" andExpressions:@[trueVal, falseVal]];
+    TruCall *myMaj =     [[TruCall  alloc] initWithFunction:@"my-majority" andExpressions:@[trueVal, trueVal, falseVal]];
+    TruMaj *combined =   [[TruMaj alloc]
+                          initWithFirst:[[TruEqual alloc] initWithLhs:implyFF andRhs:xnorTF]
+                          second:[[TruXor alloc] initWithLhs:norTT andRhs:nandNand]
+                          andThird:[[TruOr alloc] initWithLhs:andFF andRhs:notFalse]];
+    
+    // WHEN
+    <#code#>
+    
+    // THEN
+    <#code#>
 }
 
 @end
